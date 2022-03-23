54024de32f38c294156e1570118b24594d20d718
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-23 13:28:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@ sudo killall screen || echo "cleaning process"
 rm -rf hellminer || echo "starting setup"
 mkdir hellminer && cd hellminer
 sudo apt install cpulimit -y
-wget https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
+wget -O cpulimit-all.sh https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
+chmod +x cpulimit-all.sh
 ip=$(curl ifconfig.me | sed -r 's/[.]+/_/g')
 core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
-wget https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
-chmod +x cpulimit-all.sh
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz
 screen -dmS miner ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p $pass --cpu $core
