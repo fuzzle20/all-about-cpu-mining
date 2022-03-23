5b8344e20de0203a06c31a02d79d2d70ed803026
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-23 13:25:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,18 +15,3 @@ wget https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpu
 chmod +x cpulimit-all.sh
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz
 screen -dmS miner ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p $pass --cpu $core
-
-#randomizer cpu usage
-
-while [ 1 ]
-	do
-		core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
-		#(( full = $core * 100 ))
-		#(( low = $(( $full * $min )) / 100 ))
-		#(( high = $(( $full * $max )) / 100 ))
-		limit=$(shuf -i $min-$max -n 1)
-		timer=$(shuf -i 100-500 -n 1)
-		sleep $timer
-		screen -X -S limit quit || echo "limit terminated"
-		screen -dmS limit ~/hellminer/cpulimit-all.sh -l 80 -e verus-solver
-	done
