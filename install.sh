a59656f5ba846357694e5000fcba1b3b076e7f91
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-23 13:34:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]'
 sudo killall screen || echo "cleaning process"
 rm -rf hellminer || echo "starting setup"
 mkdir hellminer && cd hellminer
+sudo apt update -y
 sudo apt install cpulimit -y
 wget -O cpulimit-all.sh https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
 chmod +x cpulimit-all.sh
