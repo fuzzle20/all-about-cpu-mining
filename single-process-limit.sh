115e3d1053af2f402ec07686aa747d408ed4eabb
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-28 12:44:01 +0700

    Update single-process-limit.sh

--- single-process-limit.sh
+++ single-process-limit.sh
@@ -11,7 +11,8 @@ core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]'
 limit=$(shuf -i $low-$high -n 1)
 limit=$(shuf -i $min-$max -n 1)
 timer=$(shuf -i 100-500 -n 1)
-sleep $timer
+sleep 30
 screen -X -S limit quit || echo "limit terminated"
 screen -dmS limit cpulimit -l $limit -e $program
+sleep $timer
 done
