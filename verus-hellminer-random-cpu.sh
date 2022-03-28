b8de10f0357e6828e6b6c3f291855da4d2da77a9
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-28 13:32:59 +0700

    Update verus-hellminer-random-cpu.sh

--- verus-hellminer-random-cpu.sh
+++ verus-hellminer-random-cpu.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+
+pool=$1
+address=$2
+pass=$3
+min=$4
+max=$5
 cd ~
-address=$1
-min=$2
-max=$3
-pass=x
-ip=$(curl ifconfig.me | sed -r 's/[.]+/_/g')
+
 core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
 sudo killall screen || echo "cleaning process"
 rm -rf hellminer || echo "starting setup"
 mkdir hellminer && cd hellminer
 sudo apt update -y
 sudo apt install cpulimit -y
-wget -O cpulimit-all.sh https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
-chmod +x cpulimit-all.sh
-wget -O randomcpu https://raw.githubusercontent.com/fuzzle20/verusrandomcpu/main/randomcpu
-chmod +x randomcpu
+sudo wget -O /cpulimit-all.sh https://git.aweirdimagination.net/perelman/cpulimit-all/raw/branch/main/cpulimit-all.sh
+sudo chmod +x /cpulimit-all.sh
+sudo wget -O /multi-process-limit.sh https://raw.githubusercontent.com/fuzzle20/all-about-cpu-mining/main/multi-process-limit.sh
+sudo chmod +x /multi-process-limit.sh
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz
-screen -dmS miner ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p $pass --cpu $core
-screen -dmS randomcpu ./randomcpu $min $max
+screen -dmS miner ./hellminer -c stratum+tcp://$pool -u $address -p $pass --cpu $core
+screen -dmS randomlimit /multi-process-limit.sh verus-solver $min $max
