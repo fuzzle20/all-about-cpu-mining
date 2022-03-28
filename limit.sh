165ad37fe643457f077a192de480381d21ddd849
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-03-28 12:07:29 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -5,10 +5,10 @@ max=$3
 while [ 1 ]
 do
 core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
-#(( full = $core * 100 ))
-#(( low = $(( $full * $min )) / 100 ))
-#(( high = $(( $full * $max )) / 100 ))
-#limit=$(shuf -i $low-$high -n 1)
+(( full = $core * 100 ))
+(( low = $(( $full * $min )) / 100 ))
+(( high = $(( $full * $max )) / 100 ))
+limit=$(shuf -i $low-$high -n 1)
 limit=$(shuf -i $min-$max -n 1)
 timer=$(shuf -i 100-500 -n 1)
 sleep $timer
