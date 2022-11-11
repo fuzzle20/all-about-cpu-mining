b0ccd60caf7ac9b30dd36a290cec675b85720095
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2022-11-11 19:05:42 +0700

    fix command run ssh

--- allowssh.sh
+++ allowssh.sh
@@ -1,2 +1,2 @@
-sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
+sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys
 sudo echo 'PermitRootLogin prohibit-password' | sudo tee -a /etc/ssh/sshd_config
